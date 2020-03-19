13423fc824c484ab4efd599a2ad78405b2bfe203
MichaC <kletterer@web.de>
2020-03-19 04:01:52 +0100

    Some UI updates

--- UwpApp/MainPage.xaml.cs
+++ UwpApp/MainPage.xaml.cs
@@ -1,21 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Text;
 using System.Threading.Tasks;
 using DnsClient;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Media;
+using Windows.UI;
+using System.Net;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace UwpApp
 {
+    public class HostViewModel : INotifyPropertyChanged
+    {
+        private string queryResult;
+
+        private string logResult;
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public HostViewModel()
+        {
+            this.QueryResult = "";
+            this.LogResult = "initializing..." + Environment.NewLine;
+        }
+
+        public string QueryResult
+        {
+            get { return this.queryResult; }
+            set
+            {
+                this.queryResult = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public string LogResult
+        {
+            get { return this.logResult; }
+            set
+            {
+                this.logResult = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
         private LookupClient _client;
+
+        public HostViewModel ViewModel { get; }
+
+        private ObservableCollection<QueryType> _queryTypes;
 
         public MainPage()
         {
@@ -25,37 +79,192 @@ namespace UwpApp
             {
                 EnableAuditTrail = true,
                 ThrowDnsErrors = false,
+                Retries = 0,
+                Timeout = TimeSpan.FromSeconds(5),
+                ContinueOnDnsError = true,
+                ContinueOnEmptyResponse = true,
                 UseCache = false
             });
+
+            selectQueryType.Loaded += (ev, args) =>
+            {
+                selectQueryType.SelectedIndex = 0;
+            };
+
+            this.ViewModel = new HostViewModel();
+
+            _queryTypes = new ObservableCollection<QueryType>(GetQueryTypes());
+
+            DnsClient.Tracing.Source.Switch.Level = SourceLevels.All;
+            DnsClient.Tracing.Source.Listeners.Add(new LogTraceListener(this.logOutput, scrollLog));
         }
 
-        private async void txtQuery_KeyDown(object sender, KeyRoutedEventArgs e)
+        private class LogTraceListener : TraceListener
         {
-            if (e.Key == Windows.System.VirtualKey.Enter)
+            private readonly ScrollViewer scrollLog;
+            private StringBuilder buffer;
+
+            public LogTraceListener(TextBlock logBlock, ScrollViewer scrollLog)
             {
-                var query = (sender as TextBox).Text;
-                await RunQuery(query ?? "");
+                LogBlock = logBlock;
+                this.scrollLog = scrollLog;
+                this.buffer = new StringBuilder();
+
+                _ = Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
+                    Windows.UI.Core.CoreDispatcherPriority.Normal,
+                    async () =>
+                    {
+                        while (true)
+                        {
+                            await Task.Delay(333);
+
+                            string copy;
+                            lock (this.buffer)
+                            {
+                                copy = this.buffer.ToString();
+                                this.buffer.Clear();
+                            }
+
+                            if (!string.IsNullOrEmpty(copy))
+                            {
+                                UpdateLogResult(copy);
+                            }
+                        }
+                    });
+            }
+
+            public TextBlock LogBlock { get; }
+
+            public override void Write(string message)
+            {
+                lock (this.buffer)
+                {
+                    this.buffer.Append(message);
+                }
+            }
+
+            public override void WriteLine(string message)
+            {
+                lock (this.buffer)
+                {
+                    this.buffer.AppendLine(message);
+                }
+            }
+
+            private void UpdateLogResult(string newText)
+            {
+                foreach (var line in newText
+                    .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(p => p.StartsWith("DnsClient ") ? p.Replace("DnsClient ", "") : p))
+                {
+                    var span = new Span() { };
+                    var value = new Run { Text = line };
+
+                    if (line.StartsWith("Verbose"))
+                    {
+                        value.Foreground = new SolidColorBrush(Colors.LightGray);
+                    }
+                    else if (line.StartsWith("Information"))
+                    {
+                        value.Foreground = new SolidColorBrush(Colors.LightSteelBlue);
+                    }
+                    else if (line.StartsWith("Warning"))
+                    {
+                        value.Foreground = new SolidColorBrush(Colors.Orange);
+                    }
+                    else if (line.StartsWith("Error"))
+                    {
+                        value.Foreground = new SolidColorBrush(Colors.OrangeRed);
+                    }
+
+                    span.Inlines.Add(value);
+                    span.Inlines.Add(new LineBreak());
+                    this.LogBlock.Inlines.Add(span);
+                }
+
+                // this.viewModel.LogResult += newText;
+                this.scrollLog.UpdateLayout();
+                this.scrollLog.ChangeView(0.0f, double.MaxValue, 1.0f);
             }
         }
 
-        private async void cmdQuery_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private static IEnumerable<QueryType> GetQueryTypes()
         {
-            var query = txtQuery.Text;
-            await RunQuery(query ?? "");
+            foreach (var type in Enum.GetValues(typeof(QueryType)))
+            {
+                yield return (QueryType)type;
+            }
         }
 
-        private async Task RunQuery(string query)
+        private async void TxtQueryKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == Windows.System.VirtualKey.Enter)
+            {
+                await PrepQuery();
+            }
+        }
+
+        private async void QueryClick(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            await PrepQuery();
+        }
+
+        private async Task PrepQuery()
+        {
+            var server = txtServer.Text;
+
+            IPAddress serverIp = null;
+            if (server != null)
+            {
+                try
+                {
+                    if (!IPAddress.TryParse(server, out serverIp))
+                    {
+                        var serverResult = await _client.QueryAsync(server, QueryType.A);
+                        serverIp = serverResult.Answers.AddressRecords().FirstOrDefault()?.Address;
+                    }
+                }
+                catch { }
+            }
+
+            var query = txtQuery.Text;
+
+            var typeString = selectQueryType.SelectedItem == null ? "A" : selectQueryType.SelectedItem.ToString();
+            var type = Enum.TryParse<QueryType>(typeString, out var queryType) ? queryType : QueryType.A;
+            await RunQuery(query ?? "", type, serverIp);
+        }
+
+        private async Task RunQuery(string query, QueryType queryType = QueryType.A, IPAddress server = null)
         {
             this.txtQuery.IsEnabled = false;
             try
             {
-                var result = await _client.QueryAsync(query, QueryType.ANY);
+                txtOutput.Inlines.Clear();
+                var result = server == null ?
+                    await _client.QueryAsync(query, queryType) :
+                    await _client.QueryServerAsync(new[] { server }, query, queryType);
 
-                this.txtOutput.Text = result.AuditTrail;
+                foreach (string line in result.AuditTrail.Split(Environment.NewLine))
+                {
+                    var span = new Span() { };
+                    var value = new Run { Text = line };
+                    if (line.StartsWith(";"))
+                    {
+                        value.Foreground = new SolidColorBrush(Colors.LightGray);
+                    }
+                    if (line.StartsWith(";;"))
+                    {
+                        value.Foreground = new SolidColorBrush(Colors.DarkGray);
+                    }
+
+                    span.Inlines.Add(value);
+                    span.Inlines.Add(new LineBreak());
+                    txtOutput.Inlines.Add(span);
+                }
             }
             catch (Exception ex)
             {
-                this.txtOutput.Text = ex.InnerException?.ToString() ?? ex.ToString();
+                this.ViewModel.QueryResult = ex.InnerException?.ToString() ?? ex.ToString();
             }
             finally
             {
@@ -63,24 +272,23 @@ namespace UwpApp
             }
         }
 
-        private void cmdAddresses_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private void AddressesClick(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            this.txtOutput.Text = string.Empty;
             try
             {
                 var fromFramework = NetworkInterface.GetAllNetworkInterfaces();
 
-                this.txtOutput.Text += $@"
+                this.ViewModel.QueryResult = $@"
 ;; NetworkInterface.GetAllNetworkInterfaces
 {string.Join("\r\n ", fromFramework.Select(p => p.Name + "\t" + p.NetworkInterfaceType + "\t" + p.GetPhysicalAddress() + "\t" + p.OperationalStatus))}
 
-{string.Join(", ", fromFramework.Select(p => p.GetIPProperties().DnsAddresses))}
+{string.Join(", ", fromFramework.SelectMany(p => p.GetIPProperties().DnsAddresses))}
 ;
 ";
             }
             catch (Exception ex)
             {
-                this.txtOutput.Text += $@"
+                this.ViewModel.QueryResult += $@"
 ;; NetworkInterface.GetAllNetworkInterfaces failed: {ex.Message}.
 ;";
             }
@@ -88,7 +296,7 @@ namespace UwpApp
             try
             {
                 var dnsClientNameservers = NameServer.ResolveNameServers(true, false);
-                this.txtOutput.Text += $@"
+                this.ViewModel.QueryResult += $@"
 ;; NameServer.ResolveNameServers
 {string.Join(", ", dnsClientNameservers)}
 ;
@@ -96,7 +304,7 @@ namespace UwpApp
             }
             catch (Exception ex)
             {
-                this.txtOutput.Text += $@"
+                this.ViewModel.QueryResult += $@"
 ;; NameServer.ResolveNameServers failed: {ex.Message}.
 ;";
             }
@@ -107,7 +315,7 @@ namespace UwpApp
 
                 var info = Interop.IpHlpApi.FixedNetworkInformation.GetFixedNetworkInformation();
 
-                this.txtOutput.Text += $@"
+                this.ViewModel.QueryResult += $@"
 ;; Native implementation
 {string.Join("\r\n", interfaces.Select(p => p.Name + "\t" + p.NetworkInterfaceType + "\t" + p.GetPhysicalAddress() + "\t" + p.OperationalStatus))}
 ;;{info.DomainName}
@@ -117,16 +325,15 @@ namespace UwpApp
             }
             catch (Exception ex)
             {
-                this.txtOutput.Text += $@"
+                this.ViewModel.QueryResult += $@"
 ;; Native implementation failed: {ex.Message}.
 ;";
             }
         }
-    }
 
-    internal static class IPAddressParserStatics
-    {
-        public const int IPv4AddressBytes = 4;
-        public const int IPv6AddressBytes = 16;
+        private async void SelectQueryTypeSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            await PrepQuery();
+        }
     }
 }
